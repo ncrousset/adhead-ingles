ec4b8b40a8b49639af6feaabd5a5341f4a0f7c91
Rudys Natanael Acosta Crousset <natanael926@gmail.com>
2021-02-10 02:33:16 -0500

    Reset password

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,34 +1,50 @@
 <x-guest-layout>
-    <h2>Forgot password</h2>
 
-    @if ($errors->any())
-        <div>
-            <div>Something went wrong!</div>
+    <!-- component -->
+    <div class="flex items-center w-full mt-5 md:mt-20 p-4 lg:justify-center">
 
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
+        <div
+            class="flex flex-col w-full overflow-hidden bg-white max md:shadow-lg md:flex-row md:flex-1 lg:max-w-screen-md">
+
+            <div class="p-5 bg-white md:flex-1">
+                <h3 class="my-4 text-2xl font-semibold text-gray-700">Forgot password</h3>
+
+                @if ($errors->any())
+                    <div>
+                        <div>Something went wrong!</div>
+
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
+
+                @if (session('status'))
+                    <div>
+                        {{ session('status') }}
+                    </div>
+                @endif
+
+                <form action="{{ route('password.email') }}" method="POST" class="flex flex-col space-y-5">
+                    @csrf
+
+
+                    <div class="flex flex-col space-y-1">
+                        <label for="email" class="text-sm font-semibold text-gray-500">Email address</label>
+                        <input type="email" id="email" name="email" autofocus value="{{ old('email') }}"
+                            class="px-4 py-2 transition duration-300 border border-gray-300 rounded focus:border-transparent focus:outline-none focus:ring-4 focus:ring-blue-200" />
+                    </div>
+
+                    <div>
+                        <button
+                            class="w-full px-4 py-2 text-lg font-semibold text-white transition-colors duration-300 bg-primary rounded-md shadow hover:bg-primary-dark focus:outline-none focus:ring-blue-200 focus:ring-4">
+                            Email Password Reset Link
+                        </button>
+                    </div>
+                </form>
+            </div>
         </div>
-    @endif
-
-    @if (session('status'))
-        <div>
-            {{ session('status') }}
-        </div>
-    @endif
-
-    <form action="{{ route('password.email') }}" method="POST">
-        @csrf
-        <div>
-            <label for="email">Email</label>
-            <input type="text" id="email" name="email" value="{{ old('email') }}" autofocus>
-        </div>
-
-        <div>
-            <button>Email Password Reset Link</button>
-        </div>
-    </form>
-
+    </div>
 </x-guest-layout>
--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,47 +1,60 @@
 <x-guest-layout>
-    <h2>Forgot password</h2>
+    <div class="flex items-center w-full mt-5 md:mt-20 p-4 lg:justify-center">
+        <div
+            class="flex flex-col w-full overflow-hidden bg-white max md:shadow-lg md:flex-row md:flex-1 lg:max-w-screen-md">
 
-    @if ($errors->any())
-        <div>
-            <div>Something went wrong!</div>
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
+            <div class="p-5 bg-white md:flex-1">
+                <h3 class="my-4 text-2xl font-semibold text-gray-700">Forgot password</h3>
+
+                @if ($errors->any())
+                    <div>
+                        <div>Something went wrong!</div>
+
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
+
+                @if (session('status'))
+                    <div>
+                        {{ session('status') }}
+                    </div>
+                @endif
+
+                <form action="/reset-password" method="POST" class="flex flex-col space-y-5">
+                    @csrf
+                    <input type="hidden" name="token" value="{{ $request->route('token') }}">
+
+                    <div class="flex flex-col space-y-1">
+                        <label for="email" class="text-sm font-semibold text-gray-500">Email address</label>
+                        <input type="email" id="email" name="email" autofocus value="{{ old('email') }}"
+                            class="px-4 py-2 transition duration-300 border border-gray-300 rounded focus:border-transparent focus:outline-none focus:ring-4 focus:ring-blue-200" />
+                    </div>
+
+                    <div class="flex flex-col space-y-1">
+                        <label for="password" class="text-sm font-semibold text-gray-500">Password</label>
+                        <input type="password" id="password" name="password" autocomplete="new-password"
+                            class="px-4 py-2 transition duration-300 border border-gray-300 rounded focus:border-transparent focus:outline-none focus:ring-4 focus:ring-blue-200" />
+                    </div>
+                    <div class="flex flex-col space-y-1">
+                        <label for="password_confirmation" class="text-sm font-semibold text-gray-500">Confirm
+                            Password</label>
+                        <input type="password" id="password_confirmation" name="password_confirmation"
+                            autocomplete="new-password"
+                            class="px-4 py-2 transition duration-300 border border-gray-300 rounded focus:border-transparent focus:outline-none focus:ring-4 focus:ring-blue-200" />
+                    </div>
+
+                    <div>
+                        <button
+                            class="w-full px-4 py-2 text-lg font-semibold text-white transition-colors duration-300 bg-primary rounded-md shadow hover:bg-primary-dark focus:outline-none focus:ring-blue-200 focus:ring-4">
+                            Reset Password
+                        </button>
+                    </div>
+                </form>
+            </div>
         </div>
-    @endif
-
-    @if (session('status'))
-        <div>
-            {{ session('status') }}
-        </div>
-    @endif
-
-    <form action="/reset-password" method="POST">
-        @csrf
-
-        <input type="hidden" name="token" value="{{ $request->route('token') }}">
-
-        <div>
-            <label for="email">Email</label>
-            <input type="email" id="email" name="email" value="{{ old('email', $request->email) }}" autofocus>
-        </div>
-
-        <div>
-            <label for="password">Password</label>
-            <input type="password" id="password" name="password">
-        </div>
-
-        <div>
-            <label for="password_confirmation">Password Confirmation</label>
-            <input type="password" id="password_confirmation" name="password_confirmation">
-        </div>
-
-        <div>
-            <button>Reset Password</button>
-        </div>
-
-    </form>
-
+    </div>
 </x-guest-layout>
